745bf0a0220ad7bd74d69c84867da033871961b6
DavidSaruni <sarunidavid11126@gmail.com>
2024-10-05 08:26:28 +0300

    feat(FHIR SDC) add FHIR Structured Data Capture LLibrary to the proect

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // FHIR Data Capture Library
+    implementation("com.google.android.fhir:data-capture:1.0.0")
+    implementation("androidx.fragment:fragment-ktx:1.5.5")
 }
